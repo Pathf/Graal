884118bd983e4a80e61059ee904b57424750ae05
Valentin Henique <vhenique@gmail.com>
2025-08-17 16:57:55 +0200

    Fix honor/h

--- events/player_entering_world.lua
+++
@@ -1,7 +0,0 @@
-local REGISTERS = GRAAL.Event.registers
----
-
-local eventName = "PLAYER_ENTERING_WORLD"
-local eventAction = function() GRAAL.Data.timeSinceCreatePlayer = GetTime() end
-
-table.insert(REGISTERS, { name = eventName, action = eventAction })
